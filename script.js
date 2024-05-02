b832b049397abee36b2269f43c1681800dc9f5e1
Tolik <135510872+Tolik0001@users.noreply.github.com>
2024-05-02 19:31:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,43 +6,96 @@ function randomInteger(min, max) {
 let blocks = document.querySelectorAll(".block")
 let counter = 0
 let num = randomInteger(1,5)
+
+
 blocks.forEach(block=>{
+    
+//Заполнение клеток бомбами и единицами
     if(num == 0){
         num = randomInteger(1,5)
         block.innerHTML= "x"
         let arr = []
         // Добавляем элемент слева и справа
-        arr.push(blocks[counter-1])
-        arr.push(blocks[counter+1])
         // Элемент слева и снизу
         if(counter>=10){
+
             arr.push(blocks[counter-10])
-            arr.push(blocks[counter-11])
+            //console.log(String(counter).includes("0"))
+            if(counter != 0 && counter != 10 && counter != 20 && counter != 30 && counter != 40 && counter != 50 && counter != 60 && counter != 70 ){
+                arr.push(blocks[counter-11])  
+            }if( counter != 0 && counter != 9 && counter != 19 && counter != 29 && counter != 39 && counter != 49 && counter != 59 && counter != 69 && counter != 79 ){
+                arr.push(blocks[counter-9])  }
         }
         if(counter<70){
             arr.push(blocks[counter+10])
-            arr.push(blocks[counter+9])
+            if(counter != 0 && counter != 10 && counter != 20 && counter != 30 && counter != 40 && counter != 50 && counter != 60 && counter != 70 ){
+                arr.push(blocks[counter+9])  
+            }if( counter != 0 && counter != 9 && counter != 19 && counter != 29 && counter != 39 && counter != 49 && counter != 59 && counter != 69 && counter != 79 ){
+                arr.push(blocks[counter+11])  }
         }
-        if(counter!=0 && counter!= 10 && counter!=20 && counter!=30 && counter!=40 && counter!=50 && counter!=60 && counter!=70 &&){
-            arr.push(blocks[counter-9])
-            arr.push(blocks[counter+11])
-        }
-        arr.forEach(el =>{
-            if (el.innerHTML == 'x'){
-
-            }else{
-                el.innerHTML = '1'
-            }
-        })
-
+        if(counter != 0 && counter != 10 && counter != 20 && counter != 30 && counter != 40 && counter != 50 && counter != 60 && counter != 70 ){
+            arr.push(blocks[counter-1])  
+        }if( counter != 0 && counter != 9 && counter != 19 && counter != 29 && counter != 39 && counter != 49 && counter != 59 && counter != 69 && counter != 79 ){
+            arr.push(blocks[counter+1])  
+            arr.push(blocks[counter+1])  }
+            console.log(arr)
+            arr.forEach(el =>{
+                if (el.innerHTML == 'x'){
+        
+                }else{
+                    el.innerHTML = '1'
+                }
+            })
     }else{num--}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Клик по клетке
+    
     block.addEventListener('click',function(e){
         console.log('click');
-        block.style.backgroundColor = "red"
+
+console.log(blocks[counter])
+        if(block.innerHTML != "x"){
+            blocks[counter].classList.remove("hidden")
+            if(counter>=10){
+                blocks[counter-10].classList.remove("hidden")
+                //console.log(String(counter).includes("0"))
+                if(counter != 0 && counter != 10 && counter != 20 && counter != 30 && counter != 40 && counter != 50 && counter != 60 && counter != 70 ){
+                    blocks[counter-11].classList.remove("hidden")
+                }if( counter != 0 && counter != 9 && counter != 19 && counter != 29 && counter != 39 && counter != 49 && counter != 59 && counter != 69 && counter != 79 ){
+                    blocks[counter-9].classList.remove("hidden")
+                }}
+            if(counter<70){
+                if(counter != 0 && counter != 10 && counter != 20 && counter != 30 && counter != 40 && counter != 50 && counter != 60 && counter != 70 ){  
+                    blocks[counter+9].classList.remove("hidden")
+                }if( counter != 0 && counter != 9 && counter != 19 && counter != 29 && counter != 39 && counter != 49 && counter != 59 && counter != 69 && counter != 79 ){
+                    blocks[counter+11].classList.remove("hidden")
+                }}
+            if(counter != 0 && counter != 10 && counter != 20 && counter != 30 && counter != 40 && counter != 50 && counter != 60 && counter != 70 ){  
+                blocks[counter-1].classList.remove("hidden")
+            }if( counter != 0 && counter != 9 && counter != 19 && counter != 29 && counter != 39 && counter != 49 && counter != 59 && counter != 69 && counter != 79 ){
+                blocks[counter+1].classList.remove("hidden")
+            }else{
+            //Проигрыш
+        }}
     });
     counter++
 
 })
+
+
 
 
 
