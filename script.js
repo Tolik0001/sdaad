ff3e1940c8825113a64c433d210dc1ab65717ed6
Tolik <135510872+Tolik0001@users.noreply.github.com>
2024-05-12 16:31:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ function randomInteger(min, max) {
     return Math.floor(rand);
   }
 let blocks = document.querySelectorAll(".block")
+let restart = document.querySelector(".Gameover")
+let restartbtn = document.querySelector(".restart")
 let counter = 0
 let num = randomInteger(1,5)
 
 
 blocks.forEach(block=>{
+    block.setAttribute('id',counter)
     
 //Заполнение клеток бомбами и единицами
     if(num == 0){
@@ -49,24 +52,12 @@ blocks.forEach(block=>{
     }else{num--}
     
 
-
-
-
-
-
-
-
-
-
-
-
+    console.log(blocks[counter]);
 
     //Клик по клетке
-    
-    block.addEventListener('click',function(e){
-        console.log('click');
-
-console.log(blocks[counter])
+    blocks[counter].addEventListener('click',function(){
+        
+        console.log(counter);  
         if(block.innerHTML != "x"){
             blocks[counter].classList.remove("hidden")
             if(counter>=10){
@@ -94,6 +85,58 @@ console.log(blocks[counter])
     counter++
 
 })
+
+
+
+
+
+function setEvent(){
+    blocks.forEach(block =>{
+        let counter = block.id
+        console.log(blocks[counter]);
+
+    //Клик по клетке
+    block.addEventListener('click',function(){
+        
+        console.log(counter);  
+        if(block.innerHTML != "x"){
+            blocks[counter].classList.remove("hidden")
+            if(counter>=10){
+                blocks[counter-10].classList.remove("hidden")
+                //console.log(String(counter).includes("0"))
+                if(counter != 0 && counter != 10 && counter != 20 && counter != 30 && counter != 40 && counter != 50 && counter != 60 && counter != 70 ){
+                    blocks[counter-11].classList.remove("hidden")
+                }if( counter != 0 && counter != 9 && counter != 19 && counter != 29 && counter != 39 && counter != 49 && counter != 59 && counter != 69 && counter != 79 ){
+                    blocks[counter-9].classList.remove("hidden")
+                }}
+            if(counter<70){
+                if(counter != 0 && counter != 10 && counter != 20 && counter != 30 && counter != 40 && counter != 50 && counter != 60 && counter != 70 ){  
+                    blocks[counter+9].classList.remove("hidden")
+                }if( counter != 0 && counter != 9 && counter != 19 && counter != 29 && counter != 39 && counter != 49 && counter != 59 && counter != 69 && counter != 79 ){
+                    blocks[counter+11].classList.remove("hidden")
+                }}
+            if(counter != 0 && counter != 10 && counter != 20 && counter != 30 && counter != 40 && counter != 50 && counter != 60 && counter != 70 ){  
+                blocks[counter-1].classList.remove("hidden")
+            }if( counter != 0 && counter != 9 && counter != 19 && counter != 29 && counter != 39 && counter != 49 && counter != 59 && counter != 69 && counter != 79 ){
+                blocks[counter+1].classList.remove("hidden")
+            }
+        
+
+        }else{
+            //Проигрыш
+            restart.style.display = "block"}
+    });
+    })
+}
+
+setEvent()
+
+
+
+
+
+
+
 
 
 
